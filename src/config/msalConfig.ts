f236d6120dc2428134864e48ee90aef23107fef8
Mahmudur Rahman <mahmudurrahman@slickdigital.io>
2024-11-11 17:08:47 +0600

    msal config updated

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -1,4 +1,4 @@
-import { Configuration } from '@azure/msal-browser';
+import { Configuration, LogLevel } from '@azure/msal-browser';
 
 const msalConfig: Configuration = {
   auth: {
@@ -7,8 +7,33 @@ const msalConfig: Configuration = {
     redirectUri: process.env.NEXT_PUBLIC_AZURE_AD_REDIRECT_URI!,
   },
   cache: {
-    cacheLocation: 'sessionStorage',
-    storeAuthStateInCookie: false,
+    cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+          default:
+            return;
+        }
+      },
+    },
   },
 };
 
