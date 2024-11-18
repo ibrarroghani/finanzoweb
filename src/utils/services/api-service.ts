a7f55d9d80ea052600be412cc6d2738fe42728c5
mithun-slickdigital <mithunrana@slickdigital.io>
2024-11-18 16:45:11 +0600

    api prefix constrant is added

--- src/utils/services/api-service.ts
+++ src/utils/services/api-service.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { API_PREFIX } from '@/config/api/constrants';
 
 const ApiService = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/${API_PREFIX}`,
 });
 
 ApiService.interceptors.request.use(
