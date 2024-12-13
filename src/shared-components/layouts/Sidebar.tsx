df70686136b6593219ef4c6dd873c90d5e23d6c7
mithun-slickdigital <mithunrana@slickdigital.io>
2024-12-13 18:20:20 +0600

    fix: total client static number to dynamic

--- src/shared-components/layouts/Sidebar.tsx
+++ src/shared-components/layouts/Sidebar.tsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
         <div className='flex flex-col px-6 py-2'>
           <div className='flex items-center justify-between'>
             <p className='text-medium font-semibold'>Client List</p>
-            <p className='text-extra-small'>Total Client: 10</p>
+            <p className='text-extra-small'>Total Client: {users.length}</p>
           </div>
 
           <InputField
